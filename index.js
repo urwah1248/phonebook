f267ecb7ec4d8210b33ea818ea3dbf1f6e5f7549
M Urwah Shahid <urwahpatel@gmail.com>
2022-12-20 18:31:50 +0500

    finished ex 3.16

--- index.js
+++ index.js
@@ -42,30 +42,28 @@ app.get('/api/persons', (req, res) => {
 // Information Page Response
 app.get('/api/info', (req, res) => {
     Person.find({}).then(person => {
-        const numberOfPersons = person.length
-        const infoPage = `
-            <p>Phonebook has info for ${numberOfPersons} people</p>
+        res.send(`
+            <p>Phonebook has info for ${person.length} people</p>
             <p>${Date()}
-        `
-        res.send(infoPage)
+        `)
     })
 })
 
 // Using id parameters for a single person's output
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
     Person.findById(req.params.id).then(person => {
         res.json(person)
     })
-    .catch(err => res.send(err))
+    .catch(err => next(err))
 })
 
 // Deleting a person using id parameter
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
     Person.findByIdAndRemove(req.params.id).then((result) => {
         response.send(result)
         response.status(204).end()
     })
-    .catch(err => res.send(err))
+    .catch(err => next(err))
 })
 
 // ID generator for adding a new person
@@ -77,7 +75,7 @@ const generateId = () => {
   }
 
 // Adding a New Person
-app.post('/api/persons', (req,res) => {
+app.post('/api/persons', (req,res, next) => {
     const body = req.body
   
     if (!body.name || !body.number) {
@@ -106,9 +104,10 @@ app.post('/api/persons', (req,res) => {
     person.save().then(() => {
         res.json(person)
     })
+    .catch(err => next(err))
 })
 
-app.put('/api/persons/:id', (req, res) => {
+app.put('/api/persons/:id', (req, res, next) => {
     const body = req.body
     
     const person = {
@@ -121,8 +120,21 @@ app.put('/api/persons/:id', (req, res) => {
       res.json(updatedPerson)
       console.log(updatedPerson);
     })
-    .catch(error => res.send(error))
+    .catch(error => next(error))
 })
+
+const errorHandler = (err, req, res, next) => {
+    console.error(err.message)
+
+    if (err.name === 'CastError') {
+        return res.status(400).send({ error: 'malformatted id' })
+    } else if (err.name === 'ValidationError') {
+        return res.status(400).json({ error: err.message })
+    }
+    next(err)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001;
 
